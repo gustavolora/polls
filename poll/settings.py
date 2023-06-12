4e3d3e0758a6aab4f88de34e4b32dba5f2ab5d2b
gustavo lora <tavoalp1999@gmail.com>
2023-06-11 21:19:17 -0500

    changes

--- poll/settings.py
+++ poll/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'polls',
         'HOST': '127.0.0.1',
         'PORT': '5432',
-        'USER': 'postgres',
+        'USER': 'polls',
         'PASSWORD': '990906',
 
     }
